f1eed755f332bef47a7eae50f1bc534f0ec5ce5c
amit-singh-19 <vinitsingh4477@gmail.com>
2025-07-08 10:25:12 +0530

    feat: initialize backend with user authentication routes and MongoDB connection
    - Added package.json with necessary dependencies for the project.
    - Created userRoute.js for handling user registration, login, updates, and deletion with appropriate middleware for authentication and authorization.
    - Set up server.js to connect to MongoDB Atlas and configure Express to use the user routes.

---
+++ server.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+
+const app = express();
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose
+//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.fxszwhs.mongodb.net/mernDB`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+app.use(express.json());
+app.use("/api/users", userRouter);
